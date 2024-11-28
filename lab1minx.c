ef53d0f204a063bd6530380f58c3a23516dab434
6704062611409 <s6704062611409@email.kmutnb.ac.th>
2024-11-28 14:47:28 +0700

    ข้อ 4
    input สองบรรทัด

--- lab1minx.c
+++ lab1minx.c
@@ -4,8 +4,8 @@ int main(){
 	int n1;
 	int n2;
 	
-	scanf("%d %d", &n1 ,&n2);
-	
+	scanf("%d",&n1);
+	scanf("%d",&n2);	
 	if(n1 > n2){
 		int total = n1 - n2;
 		printf("Number 1 more than Number 2 = %d" ,  total );		
