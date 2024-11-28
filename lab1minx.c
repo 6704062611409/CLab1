9d30c5a12558d6eee6039ff5e7edb3118634e230
6704062611409 <s6704062611409@email.kmutnb.ac.th>
2024-11-28 14:40:54 +0700

    ข้อ 2

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 int main(){
-	int x = 42;
-	int y = 64;
-	if(x > y){
-		printf(" X > Y ");
+	int n1;
+	int n2;
+	
+	printf("Input Number 1: ");
+	scanf("%d", &n1);
+	
+	printf("Input Number 2: ");
+	scanf("%d", &n2);
+	
+	if(n1 > n2){
+		int total = n1 - n2;
+		printf(  " Number 1 more than Number 2 = %d" ,  total );		
 	}else{
-		printf(" Y > X ");
+		int total = n2 - n1;
+		printf( " Number 2 more than Number 1 = %d" ,  total );
 	}
 }
+
